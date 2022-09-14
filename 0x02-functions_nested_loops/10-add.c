03c0d18625f8b5f0b3e4b3796d315c536a191d99
Kevin odo <111025280+Mattklevin@users.noreply.github.com>
2022-09-14 03:31:23 +0100

    Update1

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,16 +1,16 @@
-1./*
-2.* File: 10-add.c
-3.* Auth: kevin odo
-4.*/
-5.#include "holberton.h"
-6./**
-7.* add - Adds two integers.
-8.* @num1: The first integer to be added.
-9.* @num2: The second integer to be added.
-10.*
-11.* Return: The result of the addition.
-12.*/
-13.int add(int num1, int num2)
-14.{
-15.	return (num1 + num2);
-16. }
+/*
+ * File: 10-add.c
+ * Auth: Muhammed Abdulrahaman Adinoyi
+ */
+#include "main.h"
+/**
+ * add - Adds two integers.
+ * @num1: The first integer to be added.
+ * @num2: The second integer to be added.
+ *
+ * Return: The result of the addition.
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
