a3d91f37e40baac04fc23ffe135fd9703823c451
Kevin odo <111025280+Mattklevin@users.noreply.github.com>
2022-09-14 03:19:16 +0100

    Update1

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,23 @@
 
-1. /*
-2. * File: 2-print_alphabet_x10.c
-3. * Auth: kevin odo
-4. */
+/*
+ * File: 2-print_alphabet_x10.c
+ * Auth: Muhammed Abdulrahaman Adinoyi
+ */
 
-5. #include "main.h"
+#include "main.h"
 
-6. /**
-7. * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
-8. */
-9. void print_alphabet_x10(void)
-10. {
-11. 	int count = 0;
-12. 	char letter;
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+ */
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char letter;
 
-13. 	while (count++ <= 9)
-14. 	{
-15. 		for (letter = 'a'; letter <= 'z'; letter++)
-16. 			_putchar(letter);
-17. 		_putchar('\n');
-18.	}
-19. }
-20. Footer
+	while (count++ <= 9)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
+}
