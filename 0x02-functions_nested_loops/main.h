02f6db6764bdbb6629a28562a37301084ab13b7c
Kevin odo <111025280+Mattklevin@users.noreply.github.com>
2022-09-14 03:05:12 +0100

    update1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,26 +1,26 @@
-1. #ifndef MAIN_H
-2. #define MAIN_H
-3.
-4. /*
-5. * File: main.h
-6. * Auth: kevin odo
-7. * Desc: Header file containing declarations for all functions
-8. * used in the 0x02-functions_nested_loops directory.
-9. */
-10.
-11. int _putchar(char c);
-12. void print_alphabet(void);
-13. void print_alphabet_x10(void);
-14. int _islower(int c);
-15. int _isalpha(int c);
-16. int print_sign(int n);
-17. int _abs(int);
-18. int print_last_digit(int);
-19. void jack_bauer(void);
-20. void times_table(void);
-21. int add(int, int);
-22. void print_to_98(int n);
-23. void print_times_table(int n);
-24.
-25. #endif
-26. Footer
+ #ifndef MAIN_H
+#define MAIN_H
+
+ /*
+ * File: main.h
+ * Auth: kevin odo
+ * Desc: Header file containing declarations for all functions
+ * used in the 0x02-functions_nested_loops directory.
+ */
+
+ int _putchar(char c);
+ void print_alphabet(void);
+ void print_alphabet_x10(void);
+ int _islower(int c);
+ int _isalpha(int c);
+ int print_sign(int n);
+ int _abs(int);
+ int print_last_digit(int);
+ void jack_bauer(void);
+ void times_table(void);
+ int add(int, int);
+ void print_to_98(int n);
+ void print_times_table(int n);
+
+ #endif
+ 
