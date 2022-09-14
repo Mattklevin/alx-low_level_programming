6861db576ff5609d5c65feda1888960f258fe270
Kevin odo <111025280+Mattklevin@users.noreply.github.com>
2022-09-14 03:28:40 +0100

    Update1

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,28 +1,29 @@
-1./*
-2.* File: 8-24_hours.c
-3.* Auth: Muhammed Abdulrahaman Adinoyi
-4.*/
-5.
-6.#include "holberton.h"
-7.
-8./**
-9.* jack_bauer - Prints every minute of the day of
-10.*              Jack Bauer, starting from 00:00 to 23:59.
-11.*/
-12.void jack_bauer(void)
-13.{
-14.	int hour, minute;
-15.
-16.	for (hour = 0; hour <= 23; hour++)
-17.	{
-18.		for (minute = 0; minute <= 59; minute++)
-19.		{
-20.			_putchar((hour / 10) + '0');
-21.			_putchar((hour % 10) + '0');
-22.			_putchar(':');
-23.			_putchar((minute / 10) + '0');
-24.			_putchar((minute % 10) + '0');
-25.			_putchar('\n');
-26.		}
-27.	}
-28. }
+
+/*
+ * File: 8-24_hours.c
+ * Auth: Muhammed Abdulrahaman Adinoyi
+ */
+
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day of
+ *              Jack Bauer, starting from 00:00 to 23:59.
+ */
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (minute = 0; minute <= 59; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
