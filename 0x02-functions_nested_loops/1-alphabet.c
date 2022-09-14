0047f3881b101cf18cc86423d43881542631b418
Kevin odo <111025280+Mattklevin@users.noreply.github.com>
2022-09-14 03:11:14 +0100

    Update1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,17 @@
- /*
- * File: 1-alphabet.c
- * Auth: kevin odo
- */
- #include "holberton.h"
- /**
- * print_alphabet - prints the alphabet in lowercase, followed by a new line.
- */
- void print_alphabet(void)
-{
-	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
-	_putchar('\n');
+#include "main.h"
+/**
+ * print_alphabet - function to print abc
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+_putchar('\n');
 }
