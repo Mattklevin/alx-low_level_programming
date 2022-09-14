63866eaeb891da85ffa2a086fffa96bcdb70ff65
Kevin odo <111025280+Mattklevin@users.noreply.github.com>
2022-09-14 03:24:41 +0100

    Update1

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,33 +1,33 @@
-1. /*
-2. * File: 5-sign.c
-3. * Auth: Muhammed Abdulrahaman Adinoyi
-4. */
-5. 
-6. #include "holberton.h"
-7.
-8. /**
-9. * print_sign - Prints the sign of a number.
-10.* @n: The number of which the sign will be printed.
-11. *
-12. * Return: 1 if the number is greater than zero,
-13. *         0 if the number is zero,
-14. *         -1 if the number is less than zero.
-15.*/
-16. int print_sign(int n)
-17. {
-18. 	if (n > 0)
-19.	{
-20.		_putchar('+');
-21.		return (1);
-22.	}
-23.	else if (n == 0)
-24.	{
-25.		_putchar('0');
-26.		return (0);
-27.	}
-28.	else
-29.	{
-30.		_putchar('-');
-31.		return (-1);
-32.	}
-33. }
+/*
+ * File: 5-sign.c
+ * Auth: Muhammed Abdulrahaman Adinoyi
+ */
+
+#include "main.h"
+
+/**
+ * print_sign - Prints the sign of a number.
+ * @n: The number of which the sign will be printed.
+ *
+ * Return: 1 if the number is greater than zero,
+ *         0 if the number is zero,
+ *         -1 if the number is less than zero.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
