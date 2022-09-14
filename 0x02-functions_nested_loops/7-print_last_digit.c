dad67a561b718c03a326d8a7ebc31ac7e082bedc
Kevin odo <111025280+Mattklevin@users.noreply.github.com>
2022-09-14 03:27:13 +0100

    Update1

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,24 +1,25 @@
-1./*
-2.* File: 7-print_last_digit.c
-3.* Auth: kevin odo
-4.*/
-5.
-6.#include "holberton.h"
-7.
-8./**
-9.* print_last_digit - Prints the last digit of a number.
-10.* @n: The number in question.
-11.*
-12.* Return: Value of the last digit.
-13.*/
-14.int print_last_digit(int n)
-15.{
-16.	int last_digit = n % 10;
-17.
-18.	if (last_digit < 0)
-19.		last_digit *= -1;
-20.
-21.	_putchar(last_digit + '0');
-22.
-23.	return (last_digit);
-24. }
+
+/*
+ * File: 7-print_last_digit.c
+ * Auth: Muhammed Abdulrahaman Adinoyi
+ */
+
+#include "main.h"
+
+/**
+ * print_last_digit - Prints the last digit of a number.
+ * @n: The number in question.
+ *
+ * Return: Value of the last digit.
+ */
+int print_last_digit(int n)
+{
+	int last_digit = n % 10;
+
+	if (last_digit < 0)
+		last_digit *= -1;
+
+	_putchar(last_digit + '0');
+
+	return (last_digit);
+}
