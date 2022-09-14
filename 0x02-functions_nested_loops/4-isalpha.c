22d160caf5e3f65da3da23076a155b8ab4cbb48d
Kevin odo <111025280+Mattklevin@users.noreply.github.com>
2022-09-14 03:23:12 +0100

    Update1

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,21 +1,21 @@
-1. /*
-2. * File: 4-isalpha.c
-3. * Auth: Muhammed Abdulrahaman Adinoyi
-4. */
-5. 
-6. #include "holberton.h"
-7. 
-8. /**
-9. * _isalpha - Checks if a character is alphabetic.
-10. * @c: The character to be checked.
-11. *
-12. * Return: 1 if character is letter, lowercase or uppercase, 0 otherwise.
-13. */
-14. int _isalpha(int c)
-15. {
-16.	if ((c >= 'a' && c <= 'z') ||
-17.	    (c >= 'A' && c <= 'Z'))
-18.		return (1);
-19.	else
-20.		return (0);
-21. }
+/*
+ * File: 4-isalpha.c
+ * Auth: Muhammed Abdulrahaman Adinoyi
+ */
+
+#include "main.h"
+
+/**
+ * _isalpha - Checks if a character is alphabetic.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is letter, lowercase or uppercase, 0 otherwise.
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') ||
+	    (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
