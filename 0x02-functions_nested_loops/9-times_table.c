58120557683adcb9339202dc95620e5e5a39b2ab
Kevin odo <111025280+Mattklevin@users.noreply.github.com>
2022-09-14 03:30:03 +0100

    Update1

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,35 +1,36 @@
-1./*
-2.* File: 9-times_table.c
-3.* Auth: Muhammed Abdulrahaman Adinoyi
-4.*/
-5.
-6.#include "holberton.h"
-7.
-8./**
-9.* times_table - Prints the 9 times table, starting with 0.
-10.*/
-11.void times_table(void)
-12.{
-13.	int num, mult, prod;
-14.
-15.	for (num = 0; num <= 9; num++)
-16.	{
-17.		_putchar('0');
-18.
-19.		for (mult = 1; mult <= 9; mult++)
-20.		{
-21.			_putchar(',');
-22.			_putchar(' ');
-23.
-24.			prod = num * mult;
-25.
-26.			if (prod <= 9)
-27.				_putchar(' ');
-28.			else
-29.				_putchar((prod / 10) + '0');
-30.
-31.			_putchar((prod % 10) + '0');
-32.		}
-33.		_putchar('\n');
-34.	}
-35. }
+
+/*
+ * File: 9-times_table.c
+ * Auth: Muhammed Abdulrahaman Adinoyi
+ */
+
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 times table, starting with 0.
+ */
+void times_table(void)
+{
+	int num, mult, prod;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
