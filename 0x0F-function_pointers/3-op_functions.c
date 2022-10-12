3941dbc6762701b4a9e2fec38defa92f24def250
Kevin odo <111025280+Mattklevin@users.noreply.github.com>
2022-10-12 16:12:11 +0100

    Update

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,53 +1,66 @@
+#include "3-calc.h"
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
 /**
-* op_add - a function that adds two ints
-* @a: int one
-* @b: int two
-* Return: returns the sum of a and b
-*/
+ * op_add - Returns the sum of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The sum of a and b.
+ */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
 /**
-* op_sub - a function that subtracts two ints
-* @a: int one
-* @b: int two
-* Return: returns the difference of a and b
-*/
+ * op_sub - Returns the difference of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The difference of a and b.
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
 /**
-* op_mul - a function that multiplies two ints
-* @a: int one
-* @b: int two
-* Return: returns the product of a and b
-*/
+ * op_mul - Returns the product of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The product of a and b.
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
 /**
-* op_div - a function that divides two ints
-* @a: int one
-* @b: int two
-* Return: returns the result of the division of a by b
-*/
+ * op_div - Returns the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The quotient of a and b.
+ */
 int op_div(int a, int b)
 {
 	return (a / b);
 }
 
 /**
-* op_mod - a function that gets the remainder of the division of two ints
-* @a: int one
-* @b: int two
-* Return: returns the remainder of the division of a by b
-*/
+ * op_mod - Returns the remainder of the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The remainder of the division of a by b.
+ */
 int op_mod(int a, int b)
 {
 	return (a % b);
