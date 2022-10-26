c23d41ab652569f4e054de8269161cdca0c934e5
Kevin odo <111025280+Mattklevin@users.noreply.github.com>
2022-10-26 02:39:12 +0100

    Update

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,21 +2,23 @@
 
 /**
  * free_listint2 - frees a linked list
- * @head: pointer to the listint_t list to be freed
+ * @head: head of a list.
+ *
+ * Return: no return.
  */
 void free_listint2(listint_t **head)
 {
-    listint_t *temp;
+	listint_t *temp;/*pointer to the temporary node address*/
+	listint_t *cur;/*pointer to the current node address*/
 
-    if (head == NULL)
-        return;
-
-    while (*head)
-    {
-        temp = (*head)->next;
-        free(*head);
-        *head = temp;
-    }
-
-    *head = NULL;
+	if (head != NULL)
+	{
+		cur = *head;/*where cur = current*/
+		while ((temp = cur) != NULL)
+		{
+			cur = cur->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
 }
