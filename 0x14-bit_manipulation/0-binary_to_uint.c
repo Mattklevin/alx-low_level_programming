3773c0c79de3ca45cbc2604eec5a17ed51cc8465
Kevin odo <111025280+Mattklevin@users.noreply.github.com>
2022-10-28 00:59:29 +0100

    Dwell

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,51 +1,25 @@
 #include "main.h"
 
 /**
- * _strlen - returns len of a string
- * @str: string
- * Return: length of the string
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
+ *
+ * Return: the converted number
  */
-
-unsigned int _strlen(const char *str)
-{
-	unsigned int len = 0;
-
-	while (str[len] != '\0')
-	{
-		len++;
-	}
-	return (len);
-}
-
-/**
- * binary_to_uint - Converts a binary number to an unsigned int
- * @b: string with 0 and 1 to convert
- * Return: converted number or 0 if there is no 0 or 1 to convert
- */
-
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int x2 = 1, conv_val = 0, len;
-	int i;
+    int i;
+    unsigned int dec_val = 0;
 
-	if (b == NULL)
-	{
-		return (0);
-	}
+    if (!b)
+        return (0);
 
-	len = _strlen(b);
+    for (i = 0; b[i]; i++)
+    {
+        if (b[i] < '0' || b[i] > '1')
+            return (0);
+        dec_val = 2 * dec_val + (b[i] - '0');
+    }
 
-	for (i = len - 1; i >= 0; i--)
-	{
-		if (b[i] != '0' && b[i] != '1')
-		{
-			return (0);
-		}
-		if (b[i] == '1')
-		{
-			conv_val += x2;
-		}
-		x2 *= 2;
-	}
-	return (conv_val);
+    return (dec_val);
 }
