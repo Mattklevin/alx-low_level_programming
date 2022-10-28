4ecca927fde2feee4c1a8fa361638f378b7bac80
Kevin odo <111025280+Mattklevin@users.noreply.github.com>
2022-10-28 01:09:19 +0100

    Dwell

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,17 +1,24 @@
 #include "main.h"
 
 /**
- * set_bit - sets a bit at a given index to 1
- * @n: pointer to the number to change
- * @index: index of the bit to set to 1
- *
- * Return: 1 for success, -1 for failure
+ * set_bit - sets the value of a bit to 1 at given index.
+ * @n: pointer of type unsigned long int.
+ * @index: index of the bit to modify.
+ * Return: 1 if it worked, -1 if an erorr occurred.
  */
+
 int set_bit(unsigned long int *n, unsigned int index)
 {
-    if (index > 63)
-        return (-1);
+	unsigned int left_shift;
 
-    *n = ((1UL << index) | *n);
-    return (1);
+	if (index > 64)
+	{
+		return (-1);
+	}
+
+	left_shift = 1 << index;
+	*n = (*n | left_shift);
+
+	return (1);
+
 }
